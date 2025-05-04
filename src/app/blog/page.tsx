50a54c985d363d1e42a20246a7f5ebaa27babc85
Snikerso <snikersdro@gmail.com>
2025-05-04 15:06:48 +0200

    test

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -9,7 +9,7 @@ export default async function BlogPage() {
   const posts = await getPosts();
 
   // console.log(posts);
-
+  return <div>test</div>;
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-4xl font-bold mb-8">Blog</h1>

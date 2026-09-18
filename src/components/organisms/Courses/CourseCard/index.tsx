2854227a17b50f116f6f420eb0b51c822f8aa298
Snikerso <snikersdro@gmail.com>
2026-09-18 12:02:26 +0200

    build

--- src/components/organisms/Courses/CourseCard/index.tsx
+++ src/components/organisms/Courses/CourseCard/index.tsx
@@ -1,30 +1,22 @@
-"use client";
 import { Course } from "@/data/courses";
-import { useRouter } from "next/navigation";
 
 interface Props {
   course: Course;
 }
 
 export const CourseCard = ({ course }: Props) => {
-  const router = useRouter();
   const allLessonsLength = course.modules.reduce(
     (acc, module) => acc + module.wordkitoLesson.length,
     0
   );
   return (
-    <div
-      className="p-4 bg-white rounded-lg shadow-md cursor-pointer"
-      onClick={() => {
-        router.push(`/materialy/${course.url}`);
-      }}
-    >
+    <article className="rounded-lg bg-white p-4 shadow-md">
       <h3 className="text-xl font-bold">{course.name}</h3>
       <div className="text-gray-700">{course.description.slice(0, 60)}...</div>
       <div className="text-gray-700">
         Ilość modułów: {course.modules.length}
       </div>
       <div className="text-gray-700">Ilość lekcji: {allLessonsLength}</div>
-    </div>
+    </article>
   );
 };
